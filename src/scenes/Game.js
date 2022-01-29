cf72575536eb8033cdf59b45a7fd537e15b33932
SucioLoco <irelandrj@yahoo.com>
2022-01-29 17:14:30 -0500

    added collision animation- when knight hits chort knight bounces back

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -4,6 +4,20 @@ let cursors;
 let knight;
 let chort;
 
+let hit = 0
+function handlePlayerChortCollision(obj1, obj2)
+{   
+    const chort = obj1
+    const dx = knight.x - chort.x
+    const dy = knight.y - chort.y
+
+    const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(200)
+
+    knight.setVelocity(dir.x, dir.y)
+    this.hit = 1
+    
+}
+
 class Game extends Phaser.Scene{
     constructor() {
         super('game')
@@ -46,9 +60,28 @@ class Game extends Phaser.Scene{
         })
         // Chort Enemy
         chort = this.physics.add.sprite(200, 200, 'chort')
+
+        // Idle Animation
+        this.anims.create({
+            key: 'chort-idle',
+            repeat: -1,
+            frameRate: 10,
+            frames: this.anims.generateFrameNames('chort', {start: 0, end: 3, prefix: 'chort/chort_idle_anim_f', suffix: '.png' }), 
+        })
+
+        // Movement Animation
+        this.anims.create({
+            key: 'chort-movement',
+            repeat: -1,
+            frameRate: 10,
+            frames: this.anims.generateFrameNames('chort', {start: 0, end: 3, prefix: 'chort/chort_run_anim_f', suffix: '.png'})
+        })
+        chort.anims.play('chort-movement')
         
         // Collision 
         this.physics.add.collider(knight, wallLayer)
+        this.physics.add.collider(chort, knight, handlePlayerChortCollision, undefined, this)
+        
 
         // Camera
         const camera = this.cameras.main;
@@ -59,11 +92,19 @@ class Game extends Phaser.Scene{
         camera.setBounds(0, 0);
         camera.setDeadzone(5, 5)
         camera.setZoom(2)
-        
     }
-
+    
     update() {
         const speed = 100
+        if (this.hit > 0)
+        {
+            ++this.hit
+            if (this.hit > 10)
+            {
+                this.hit = 0
+            }
+            return
+        }
         
         if (cursors.left.isDown) {
             knight.anims.play('knight-movement', true)
@@ -91,5 +132,4 @@ class Game extends Phaser.Scene{
         }
     }
 }
-
 export default Game
